28a36335bac85ebcd294c44bb67712cd0aa5dd1d
Cem  Onal <42888722+8mecem8@users.noreply.github.com>
2021-03-20 14:45:29 -0500

    updated

---
+++ scroll-with-images/preExamScript.js
@@ -0,0 +1,21 @@
+
+let speed = 0;
+let position = 0;
+let box = document.getElementById('box')
+
+window.addEventListener('wheel', (e) => {
+    speed += e.deltaY * 0.2
+})
+
+
+
+function run()
+ {
+     console.log(speed)
+     
+    box.style.transform = `translate(0,${speed}px)`
+    window.requestAnimationFrame(run)
+ }
+
+
+ run()
