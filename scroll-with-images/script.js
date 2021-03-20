2b49125842420315bcbb152450cd7515e069645a
Cem  Onal <42888722+8mecem8@users.noreply.github.com>
2021-03-20 13:27:05 -0500

    updated

---
+++ scroll-with-images/script.js
@@ -0,0 +1,39 @@
+
+
+let speed = 0;
+let position = 0;
+let box = document.getElementById('box')
+
+window.addEventListener('wheel', (e) => {
+    speed += e.deltaY * 0.0002
+})
+
+
+
+function run()
+ {
+     
+     position += speed
+
+     speed*=0.94
+     
+    rounded = Math.round(position)
+
+
+    let diff = (rounded - position)
+
+    //position += diff*0.015
+
+    position += rounded *0.05
+
+    position *= 0.9524
+
+    box.style.transform = `translate(0,${position *100}px)`
+
+    
+    window.requestAnimationFrame(run)
+    console.log(position,speed,rounded)
+ }
+
+
+ run()
